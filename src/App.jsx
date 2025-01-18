1d0a95ceebecf37fe2ac83ab724c410dc88c9000
Abhishek Singh <singhabhishekj21@gmail.com>
2025-01-18 13:11:01 +0000

    project setup code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./layouts/app-layout";
+import LandingPage from "./pages/landing";
+import Onboarding from "./pages/onboarding";
+// import AppLayout from "./AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />, // Define the path and associate it with the element
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: <Onboarding />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <>learn react</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
