abf3501cc665626153a0c2aee289f71f672b4caf
Abhishek Singh <singhabhishekj21@gmail.com>
2025-01-19 13:28:57 +0000

    applied authentication feature

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
@@ -5,7 +6,11 @@ const AppLayout = () => {
   return (
     <div>
       <div className="grid-background"></div>
-      <Outlet />
+      <main className="min-h-screen container mx-auto">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-10 text-center bg-gray-800 mt-10">Get Job</div>
     </div>
   );
 };
